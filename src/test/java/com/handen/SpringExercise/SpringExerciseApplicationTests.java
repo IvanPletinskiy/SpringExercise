771b57000c2838c7d0c5143acf01dc9a71ed5df1
Handen Laptop <handeninc@gmail.com>
2019-11-03 21:07:10 +0300

    Initial commit

---
+++ src/test/java/com/handen/SpringExercise/SpringExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handen.SpringExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
